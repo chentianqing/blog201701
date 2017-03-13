93ebefb6b850324adb46bc8337be29eb4778d93b
chentianqing <993049884@qq.com>
2017-03-13 22:56:03 +0800

    add html file

--- blog201701/settings.py
+++ blog201701/settings.py
@@ -118,6 +118,19 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
+# https://docs.djangop
+# roject.com/en/1.9/howto/static-files/
 
+# STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
+#
+# STATICFILES_DIRS = (
+#     ("images", os.path.join(BASE_DIR,'static/images').replace('\\','/')),
+# )
+# STATICFILES_FINDERS = (
+#
+#     "django.contrib.staticfiles.finders.FileSystemFinder",
+#
+#     "django.contrib.staticfiles.finders.AppDirectoriesFinder"
+#
+# )
